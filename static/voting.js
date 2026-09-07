38a32f15a3caad050201c74afffb766f032c83a6
DomAbobora <adonaigalvao13@gmail.com>
2026-09-07 20:14:30 -0300

    modified:   static/voting.js

--- static/voting.js
+++ static/voting.js
@@ -85,7 +85,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const voteStage = document.getElementById('voteStage');
   const presidenciaSection = document.getElementById('presidenciaSection');
   const governadorSection = document.getElementById('governadorSection');
-
+button.addEventListener('click', async () => {
+  const usedTokens = 0; // Ensure the variable is declared and initialized
+  console.log(usedTokens); 
+});
   let currentVoterData = {};
   let voteStageName = 'presidencia';
   let presidentialVote = '';
